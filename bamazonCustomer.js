a21cb5845e81c0a374b6907ca62c73d8de2757d3
Atton <jabusau@gmail.com>
2018-08-30 20:28:25 -0700

    before resolve test

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var table = require("console.table");
+var quit = " [Enter Q to quit]";
 
 // initialize connection with database
 var connection = mysql.createConnection({
@@ -30,23 +31,91 @@ var connection = mysql.createConnection({
 
 // Read items (id, name, price, quantity)
 function readProduct() {
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function(err, results) {
       if (err) throw err;
       // Log all results of the SELECT statement
     
+      // Create table
       // NEED FORMATTING
-      console.log("Item ID  Product Name             Department Name    Price  Stock\n");
-      console.log("-------  -----------------------  -----------------  -----  -----\n");
-      console.table(["a","b","c"]);
-    
-    //   console.log(JSON.stringify(res, null, 2)); 
+      var productsArr = [];
+      for (var i = 0; i < results.length; i++){
+        productsArr.push([results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
+      }
+      console.table(["Item ID", "Product Name", "Department", "Price", "Stock"], productsArr)
       
+      // console.log(JSON.stringify(results, null, 2)); //test
+      
+      buyProduct(results);
     });
   }
-// What id product to buy
 
-// How many of product to buy
+function buyProduct(){
 
-// check if available, if not "Insufficient quantity"
+  // Ask for id and quantity of item to buy
+  inquirer.prompt([{
+    name: "id",
+    message: "What is the ID of the product you want to buy?" + quit, // adds [Enter Q to quit]
+    type: "input",
+    validate: function(id) {
+      return isNumber(id);
+    }
+  } , {
+    when: function (response) {
+      return (response.id !== "Q")
+    },
+    name: "buy_quantity",
+    message: "How many of the product do you want to buy?" + quit, // adds [Enter Q to quit]
+    type: "input",
+    validate: function(buy_quantity) {
+      return isNumber(buy_quantity);
+    }
+  }
+]). then(function(result) {
+  if (result.id === "Q" || result.buy_quantity === "Q") { // exit if user pressed Q
+    exit();
+  } else {
+    connection.query(
+      "SELECT * FROM products WHERE ?", // search for item_id
+      {
+        item_id: result.id
+      },
+      function(err, databaseResults) {
+        if (err) throw err;
 
-// connection.end();
+        // console.log(databaseResults);
+        
+        // If not enough inventory
+        if (databaseResults[0].stock_quantity < result.buy_quantity) { // check inventory
+          console.log("Insufficient Stock. Unable to buy " + result.buy_quantity + " " + databaseResults[0].product_name + "'s.");
+          buyProduct(); // return to buy screen
+
+        // If enough, buy it
+        } else {
+          console.log("You successfully purchased " + result.buy_quantity + " " + databaseResults[0].product_name + "'s!");
+          // updateProduct(result.id, result.buy_quantity)
+          buyProduct(); // return to buy screen
+        }
+      }
+    )
+  }
+})
+}
+
+function updateProduct(id, quantity){
+
+}
+
+// check if input is a number or Q
+function isNumber(str) {
+    if (str === "Q") {
+      return true;
+    }
+    if (parseFloat(str)){
+        return true;
+    }
+    return false;
+}
+
+function exit() {
+  connection.end();
+}
